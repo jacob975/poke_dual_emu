a172bf7763144b28b87ce5a493d4ccbff038fbe7
jacob975 <z123a123s123@gmail.com>
2020-08-07 21:55:04 +0800

    Minor fix

--- poke_dual_emu/pokelist_crawler.py
+++ poke_dual_emu/pokelist_crawler.py
@@ -60,14 +60,15 @@ if __name__ == "__main__":
     pokelist_wiki52page = 'https://wiki.52poke.com/zh-hant/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E5%88%97%E8%A1%A8%EF%BC%88%E6%8C%89%E5%85%A8%E5%9B%BD%E5%9B%BE%E9%89%B4%E7%BC%96%E5%8F%B7%EF%BC%89'
     page = requests.get(pokelist_wiki52page)
     root = html.fromstring(page.content.decode('UTF-8'))
-    # List the table class name
+    # List the table class name, and only take the table with pokemon infos.
     table_class_name_list = []
     for table in root.xpath("//table"):
         table_class_name = table.get('class')
         table_class_name_list.append(table_class_name)
-    print(table_class_name_list)
+    table_class_name_array = np.array(table_class_name_list)
+    table_class_name_array = np.unique(table_class_name_array)[2:10]
     # List the pokemon info in a line for each.
-    for tbname in table_class_name_list:
+    for tbname in table_class_name_array:
         obtain_poke(root, tbname)
     #-----------------------------------
     # Measure time
